bb5113bd868ebaeedaf456d3b355f5ce028e5ae2
SergioCerCam <sergioccool@gmail.com>
2013-04-02 19:58:49 +0000

    Creando ventana

--- DiamondDogs.py
+++ DiamondDogs.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 
+#-Importo las librerias:
+
 import pygame
 from pygame.locals import *
 
+#---Inicio pygame:
+pygame.init()
+
+#---Creo la ventana donde se vera el juego:
+
+ventana=(690,480)
+area = pygame.display.set_mode(ventana)
+
+#---Le pongo un titulo a la ventana del juego que se vera arriba:
+
+pygame.display.set_caption('The Phantom Game')
+
+#---Creo un bucle para que se pueda cerrar la ventana al darle a la x:
+
+salir=False
+while not salir:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            salir=True
+
 def main():
+
+#---Le pongo a return 0 para que la funcion no devuelva ningun valor:
     return 0
 if __name__== '__main__':
-    pygame.init()
     main()
 
