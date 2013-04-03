671954aa869d84a17eb242511e2dd75339e2878e
SergioCerCam <sergioccool@gmail.com>
2013-04-03 10:11:34 +0200

    Arreglo fondo ventana

--- DiamondDogs.py
+++ DiamondDogs.py
@@ -11,9 +11,18 @@ pygame.init()
 
 #---Creo la ventana donde se vera el juego:
 
-ventana=(690,480)
-area = pygame.display.set_mode(ventana)
+ventana=(600,450)
 
+#---Funcion fondo pantalla
+def load_image(filename, transparent=False):
+    try: image = pygame.image.load(filename)
+    except pygame.error, message:
+        raise SystemExit, message
+    image = image.convert()
+    if transparent:
+        color = image.get_at((0,0))
+        image.set_colorkey(color, RLEACCEL)
+    return image
 
 #---Funcion
 def main():
@@ -25,21 +34,23 @@ def main():
     pygame.display.set_caption('The Phantom Game')
 
 #---Le pongo una imagen de fondo a la ventana:
-    fondo = pygame.image.load("fondo.jpg").convert()
 
-#---Especifico la posion de la imagen de fondo:
-    screen.blit(fondo, (0, 0))
-
-    pygame.display.flip()
+    background_image = load_image('fondo.jpg')
 
 #---Creo un bucle para que se pueda cerrar la ventana al darle a la x:
 
-salir=False
-while not salir:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            salir=True
+    salir=False
+    while not salir:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                salir=True
 
+#---Especifico la posion de la imagen de fondo:
+                
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
+
+    return 0
     
 if __name__== '__main__':
     main()
